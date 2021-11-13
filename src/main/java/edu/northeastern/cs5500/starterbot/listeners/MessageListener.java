ae89bf668c11c3afe7bd20f2a6170af4ef689cb6
xxjingxx <jingxie09@gmail.com>
2021-11-13 12:37:30 -0800

    Added slashCommand placeholders Added the following slashCommand placeholders: * /help * /search * /sort * /filter

--- src/main/java/edu/northeastern/cs5500/starterbot/listeners/MessageListener.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/listeners/MessageListener.java
@@ -9,6 +9,38 @@ public class MessageListener extends ListenerAdapter {
         switch (event.getName()) {
             case "say":
                 event.reply(event.getOption("content").getAsString()).queue();
+                break;
+
+                // sort command handler
+            case "sort":
+                event.reply(
+                                "Here are the jobs sorted based on "
+                                        + event.getOption("category").getAsString())
+                        .queue();
+                // todo: change event.reply with sort function call
+                break;
+
+                // filter command handler
+            case "filter":
+                event.reply(
+                                "Here are the jobs filtered based on "
+                                        + event.getOption("category").getAsString())
+                        .queue();
+                // todo: change event.reply with filter function call
+                break;
+
+            case "help":
+                event.reply("The help menu will be added soon...").queue();
+                // todo: change event.reply with help function call
+                break;
+
+            case "search":
+                event.reply("The stuff you are searching for will be added soon...").queue();
+                // todo: change event.reply with search function call
+                break;
+
+            default:
+                event.reply("I am busy thinking important stuff now...").setEphemeral(true).queue();
         }
     }
 }
