a1f04b0663d962e44c02e7ec0cff30aae3ceb828
Alexander <167740+abl@users.noreply.github.com>
2021-11-13 20:34:25 -0800

    Implement command pattern and port /sort

---
+++ src/main/java/edu/northeastern/cs5500/starterbot/listeners/commands/SortCommand.java
@@ -0,0 +1,33 @@
+package edu.northeastern.cs5500.starterbot.listeners.commands;
+
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+public class SortCommand implements Command {
+
+    @Override
+    public String getName() {
+        return "sort";
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandEvent event) {
+        event.reply(
+                        "Here are the jobs sorted based on "
+                                + event.getOption("category").getAsString())
+                .queue();
+    }
+
+    @Override
+    public CommandData getCommandData() {
+        return new CommandData("sort", "Sort jobs based on a category.")
+                .addOptions(
+                        new OptionData(
+                                        OptionType.STRING,
+                                        "category",
+                                        "What category do you want to sort?")
+                                .setRequired(true));
+    }
+}
