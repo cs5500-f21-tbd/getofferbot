be411cde2132c46f3114d913b5a3c14c0b10b552
Poppymeow <52218955+Poppymeow@users.noreply.github.com>
2021-11-18 04:02:28 -0800

    Update Scraper.java

--- src/main/java/edu/northeastern/cs5500/ScrapeIndeed/Scraper.java
+++ src/main/java/edu/northeastern/cs5500/ScrapeIndeed/Scraper.java
@@ -1,5 +1,10 @@
+package edu.northeastern.cs5500.starterbot.scraper
+
 import java.io.IOException;
 import java.util.ArrayList;
+
+import edu.northeastern.cs5500.starterbot.model.Job;
+import edu.northeastern.cs5500.starterbot.model.Location;
 
 public interface Scraper {
     public ArrayList<Job> ScrapeLocation(String location) throws IOException, RuntimeException;
