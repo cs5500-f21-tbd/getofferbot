6c7e0917bd931f88c4c197a2b800aad09da5d07a
Poppymeow <52218955+Poppymeow@users.noreply.github.com>
2021-11-18 03:58:00 -0800

    Create Job.java

---
+++ src/main/java/edu/northeastern/cs5500/model/Job.java
@@ -0,0 +1,28 @@
+// Placeholder for task 71
+package edu.northeastern.cs5500.starterbot.model;
+
+import lombok.Data;
+import java.sql.Timestamp;
+
+
+enum Experience {
+    intern,
+    entry,
+    mid,
+    senior;
+}
+
+@Data
+public class Job {
+    private Integer JobId;
+    private String JobTitle;
+    private String JobType;
+    private Experience Experience;
+    private String Company;
+    private Timestamp Created;
+    private float AnnualPay;
+    private float StarRating;
+    private boolean Sponsorship;
+    private String LinkToApply;
+    private Location Location;
+}
