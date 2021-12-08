4a4b4a645ad8f1fbdc4a9282f749297fb70b205b
yiyuzhang0604 <yiyuzhang0604@gmail.com>
2021-12-08 12:30:58 -0800

    add a setter in user class

--- src/main/java/edu/northeastern/cs5500/starterbot/model/User.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/User.java
@@ -3,6 +3,8 @@ package edu.northeastern.cs5500.starterbot.model;
 import edu.northeastern.cs5500.starterbot.service.ZipCodeDistanceService;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -30,5 +32,10 @@ public class User implements Model {
                 (searchRadiusMiles == null)
                         ? null
                         : (searchRadiusMiles * ZipCodeDistanceService.KILOMETERS_IN_A_MILE);
+    }
+
+    // add setter to set the zipcode 
+    public void setZipcode(String zipcode) {
+        zipcode = (zipcode == null) ? null : zipcode; 
     }
 }
